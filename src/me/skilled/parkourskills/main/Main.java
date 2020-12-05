224062f8036b17e7237ca78208b758e4b7fac006
Bernat Relats Martínez <bernatrelats@gmail.com>
2020-12-05 21:58:19 +0100

    First commit

---
+++ src/me/skilled/parkourskills/main/Main.java
@@ -0,0 +1,13 @@
+package me.skilled.parkourskills.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin
+{
+    @Override
+    public void onEnable()
+    {
+        System.out.println("Enabled!");
+        super.onEnable();
+    }
+}
